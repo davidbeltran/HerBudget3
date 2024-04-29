f68a5039ff143cc2a8d2620281317b2c8e2f3f4f
David Beltran <beltrannowd5@gmail.com>
2024-04-29 16:23:32 -0700

    trying pdf library

--- src/main/java/aftergrad/herbudget/Main.java
+++ src/main/java/aftergrad/herbudget/Main.java
@@ -1,11 +1,20 @@
 package aftergrad.herbudget;
 
+
+import java.io.File; 
+import org.apache.pdfbox.pdmodel.PDDocument; 
+import org.apache.pdfbox.text.PDFTextStripper; 
 /**
  *
  * @author David Beltran
  */
 public class Main {
     public static void main(String[] args) {
-    System.out.println("hello world");
+        File file = new File("sample.pdf");
+        PDDocument document = PDDocument.load(file);
+        PDFTextStripper stripper = new PDFTextStripper();
+        String text = stripper.getText(document);
+        document.close();
+        System.out.println("hello world");
     }
 }
