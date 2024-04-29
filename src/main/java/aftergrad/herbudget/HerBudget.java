553ee9c1f05ab7de9f9cfbc621ee141a7ea24051
David Beltran <beltrannowd5@gmail.com>
2024-04-29 13:53:51 -0700

    initiate for github attempt

---
+++ src/main/java/aftergrad/herbudget/HerBudget.java
@@ -0,0 +1,12 @@
+package aftergrad.herbudget;
+
+/**
+ *
+ * @author David Beltran
+ */
+public class HerBudget {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
