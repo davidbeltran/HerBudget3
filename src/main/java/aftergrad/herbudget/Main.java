67d92b5c9c4fc7b1f60e266cce828c291feedea0
David Beltran <beltrannowd5@gmail.com>
2024-05-07 16:48:57 -0700

    made statement class with working methods on main

--- src/main/java/aftergrad/herbudget/Main.java
+++ src/main/java/aftergrad/herbudget/Main.java
@@ -1,39 +1,13 @@
 package aftergrad.herbudget;
 
-
-import java.io.File; 
 import java.io.IOException;
-import org.apache.pdfbox.pdmodel.PDDocument; 
-import org.apache.pdfbox.text.PDFTextStripper; 
-import java.util.regex.*;
-import java.util.ArrayList;
 /**
  *
  * @author David Beltran
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("NovDec.pdf");
-        PDDocument document = PDDocument.load(file);
-        PDFTextStripper stripper = new PDFTextStripper();
-        String text = stripper.getText(document);
-        document.close();
-        String regPat = "(?:\\n((?:0[1-9]|1[1,2])/(?:0[1-9]|[12][0-9]|3[01]))\\s*(.+)" +
-            " ((?:-\\d+\\.\\d{2})|(?:\\d+\\.\\d{2})))";
-        Pattern pat = Pattern.compile(regPat);
-        Matcher mat = pat.matcher(text);
-        
-        ArrayList<ArrayList> expenses = new ArrayList<>();
-        while (mat.find()) {
-            ArrayList temp = new ArrayList<>();
-            temp.add(mat.group(1));
-            temp.add(mat.group(2));
-            temp.add(Double.valueOf(mat.group(3)));
-            expenses.add(temp);
-        }
-        
-        for (ArrayList exp : expenses){
-            System.out.printf("Date: %s, Detail: %s, Amount: %.2f\n", exp.get(0), exp.get(1), exp.get(2));
-        }
+        Statement statement = new Statement("NovDec.pdf");
+        statement.getExpenses();
     }
 }
--- src/main/java/aftergrad/herbudget/Statement.java
+++ src/main/java/aftergrad/herbudget/Statement.java
@@ -1,9 +1,74 @@
 package aftergrad.herbudget;
 
+import java.util.ArrayList;
+import java.io.File;
+import org.apache.pdfbox.pdmodel.PDDocument; 
+import org.apache.pdfbox.text.PDFTextStripper; 
+import java.io.IOException; 
+import java.util.regex.Pattern; 
+import java.util.regex.Matcher;
+
 /**
  *
  * @author David Beltran
  */
 public class Statement {
+    private String pdfPath;
+    private String pdfText;
+    private String regexPattern;
+    private final ArrayList<ArrayList> expenses;
     
+    public Statement(String pdfPath) {
+        this.pdfPath = pdfPath;
+        this.regexPattern = "(?:\\n((?:0[1-9]|1[1,2])/(?:0[1-9]|[12][0-9]|3[01]))\\s*(.+)" +
+            " ((?:-\\d+\\.\\d{2})|(?:\\d+\\.\\d{2})))";
+        this.pdfText = "";
+        this.expenses = new ArrayList<>();
+    }
+    
+    private String preparePdf(String pdfPath) throws IOException{
+        File file = new File(pdfPath);
+        try (PDDocument doc = PDDocument.load(file)) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            this.pdfText = stripper.getText(doc);
+            doc.close();
+        }
+        return pdfText;
+    }
+
+    private ArrayList<ArrayList> createExpenseList() throws IOException {
+        Pattern pat = Pattern.compile(this.regexPattern);
+        Matcher mat = pat.matcher(preparePdf(this.pdfPath));
+        while (mat.find()) {
+            ArrayList temp = new ArrayList<>();
+            temp.add(mat.group(1));
+            temp.add(mat.group(2));
+            temp.add(Double.valueOf(mat.group(3)));
+            this.expenses.add(temp);
+        }
+        return this.expenses;
+    }
+    
+    public void getExpenses() throws IOException{
+        createExpenseList();
+        for (ArrayList exp : this.expenses){
+            System.out.printf("Date: %s, Detail: %s, Amount: %.2f\n", exp.get(0), exp.get(1), exp.get(2));
+        }
+    }
+    
+    public String getPdfPath() {
+        return pdfPath;
+    }
+
+    public void setPdfPath(String pdfPath) {
+        this.pdfPath = pdfPath;
+    }
+
+    public String getRegexPattern() {
+        return regexPattern;
+    }
+
+    public void setRegexPattern(String regexPattern) {
+        this.regexPattern = regexPattern;
+    }
 }
