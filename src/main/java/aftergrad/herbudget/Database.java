a1cc59cad452bac473ca53507e76cf6bb2b6f25f
David Beltran <beltrannowd5@gmail.com>
2024-05-13 13:54:25 -0700

    made first connection with MongoDB

--- src/main/java/aftergrad/herbudget/Database.java
+++ src/main/java/aftergrad/herbudget/Database.java
@@ -9,3 +9,44 @@ public class Database {
     
 }
 // https://www.mongodb.com/docs/drivers/java/sync/v4.3/quick-start/
+
+
+/*
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+public class MongoClientConnectionExample {
+    public static void main(String[] args) {
+        String connectionString = "mongodb+srv://beltrannowd5:Diska1725!@herbudgetclusterjava.f2hiz7o.mongodb.net/?retryWrites=true&w=majority&appName=HerBudgetClusterJava";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+
+mongodb+srv://beltrannowd5:Diska1725!@herbudgetclusterjava.f2hiz7o.mongodb.net/?retryWrites=true&w=majority&appName=HerBudgetClusterJava
+*/
--- src/main/java/aftergrad/herbudget/Main.java
+++ src/main/java/aftergrad/herbudget/Main.java
@@ -1,13 +1,34 @@
 package aftergrad.herbudget;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
 import java.io.IOException;
+import org.bson.Document;
 /**
  *
  * @author David Beltran
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        Statement statement = new Statement("NovDec.pdf");
-        statement.getExpenses();
+        //Statement statement = new Statement("NovDec.pdf");
+        //statement.getExpenses();
+        
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://beltrannowd5:Diska1725!@herbudgetclusterjava.f2hiz7o.mongodb.net/?retryWrites=true&w=majority&appName=HerBudgetClusterJava";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+        
+        
     }
 }
